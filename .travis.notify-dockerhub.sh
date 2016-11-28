401fdbbaa91412b8b2d793c306d338d62a230de6
Jirka Kremser <jkremser@redhat.com>
2016-11-28 17:11:41 +0100

    If travis is triggered on a build that is a 'tag-build' it notifies the dockerhub rebuild the images. Because the dockerhub autobuild feature is broken.
    (cherry picked from commit dd2c96c0881675de7875aff0d23b665d11cf9200)
    Signed-off-by: Jirka Kremser <jkremser@redhat.com>

---
+++ .travis.notify-dockerhub.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters, this script takes the desired tag as a param."
+    echo "example: ./.travis.notify-dockerhub.sh 0.24.2"
+fi
+
+tag=$1
+
+
+while read line; do
+  linearray=($line)
+  token=${linearray[0]}
+  name=${linearray[1]}
+  echo $token
+  echo $name
+  curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "'$tag'"}' -X POST https://registry.hub.docker.com/u/jkremser/$name/trigger/$token/
+
+done <./.travis.dockerhub-tokens.txt
